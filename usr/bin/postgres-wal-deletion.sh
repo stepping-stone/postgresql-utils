1d0bdd89741d835ccd12d3fdb2d67f8cbaffaba7
Yannick Denzer <yannick.denzer@stepping-stone.ch>
2017-09-07 13:17:20 +0200

    postgres-wal-deletion.sh: set minimal depth to 1 (files inside the given directory)

--- usr/bin/postgres-wal-deletion.sh
+++ usr/bin/postgres-wal-deletion.sh
@@ -3,4 +3,4 @@
 wal_dir=/var/backup/postgres/wal
 
 # delete old WAL files which were last modified 48h ago
-/usr/bin/find $wal_dir -mtime +2 -delete
+/usr/bin/find $wal_dir -mindepth 1 -mtime +2 -delete
